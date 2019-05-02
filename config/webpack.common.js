bb7760782725bb50485e4263f10e0ebd5f649c88
Lisa Ross <lisa@thisisbroad.com>
2019-05-02 11:41:29 -0400

    add babel polyfill

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -2,9 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    main: './src/main.js'
-  },
+  entry: ['@babel/polyfill', './src/main.js'],
   output: {
     filename: '[name]-bundle.js',
     path: path.resolve(__dirname, '../dist')

4a35008608a94c25f0689b8d406667a9092dec4c
Lisa Ross <lisa@thisisbroad.com>
2019-05-02 13:33:34 -0400

    optimize css and js in prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,24 +1,20 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
   devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: process.env.NODE_ENV === 'development'
-            }
-          },
-          'css-loader',
-          'sass-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },

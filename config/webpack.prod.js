01af15eb86ff9c99a1197fca96b22480726cf2f5
Lisa Ross <lisa@thisisbroad.com>
2019-05-02 12:38:55 -0400

    extract css in prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,7 +1,26 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map'
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development'
+            }
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
 });

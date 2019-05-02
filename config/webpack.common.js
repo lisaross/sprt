250aefc7c42079bac924c224c9e2c99c57fddf32
Lisa Ross <lisa@thisisbroad.com>
2019-05-02 11:19:45 -0400

    separate webpack configs

---
+++ config/webpack.common.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/main.js'
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      },
+      { test: /\.pug$/, loader: 'pug-loader' }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/views/index.pug',
+      title: 'pug demo'
+    })
+  ]
+};
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,70 +1,14 @@
-const path = require('path');
 const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
-module.exports = {
-  entry: {
-    main: './src/main.js'
-  },
+module.exports = merge(common, {
   mode: 'development',
-  output: {
-    filename: '[name]-bundle.js',
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: '/'
-  },
+  devtool: 'inline-source-map',
   devServer: {
     contentBase: 'dist',
     overlay: true,
     hot: true
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: {
-          loader: 'babel-loader'
-        },
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader'
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              attrs: ['img:src']
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|gif|png)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'images/[name].[ext]'
-            }
-          }
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HTMLWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
-};
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+});

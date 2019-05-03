cb3fda7a161d7df1fe15a9fd5e4763c186964f4e
Lisa Ross <lisa@thisisbroad.com>
2019-05-03 11:26:22 -0400

    remove hmr

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,17 +1,7 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: 'public',
-    overlay: true,
-    hot: true
-  },
-  module: {
-    rules: []
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  devtool: 'inline-source-map'
 });
